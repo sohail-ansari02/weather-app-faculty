ac95b11e746d81925b1188a2582350a08ec3e4f6
sohail-ansari02 <syansari02@gmail.com>
2024-02-12 18:54:39 +0530

    app is done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,42 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { WeatherService } from './services/weather.service';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    FormsModule,
+    MatInputModule,
+    MatIconModule,
+    MatCardModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent implements OnInit {
   title = 'weather-app';
   ws = inject(WeatherService);
+  cityName = '';
+  weather: any;
   ngOnInit(): void {
-    this.ws.getByCity().subscribe(console.log)
+    this.ws.getCurrentLocationWeather().subscribe({
+      next: (res) => this.weather = res,
+      error: () => this.weather = undefined
+    })
   }
-
+  searchByCity(name: string) {
+    this.ws.getWeatherByCity(name).subscribe({
+      next: (res) => {
+        this.weather = res;
+        // console.log(this.weather);
+      },
+      error: () => (this.weather = undefined),
+    });
+  }
 }
--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
+import { Observable, from, mergeMap, switchMap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -8,8 +9,24 @@ export class WeatherService {
   API_KEY = 'a40b36ff21e126edcc09f6c9692c9b46';
   API = `http://api.openweathermap.org/data/2.5/weather?appid=${this.API_KEY}`;
   http = inject(HttpClient);
+  position: any = undefined;
   constructor() {}
-  getByCity(name: string = 'pune') {
+  getWeatherByCity(name: string) {
     return this.http.get(`${this.API}&q=${name}`);
+  }
+  getCurrentLocationWeather() {
+    let position$ = new Observable((observer) => {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const lat = position.coords.latitude;
+        const lon = position.coords.longitude;
+        observer.next({ lat, lon });
+        observer.complete();
+      });
+    });
+    return position$.pipe(
+      switchMap((pos: any) => {
+        return this.http.get(`${this.API}&lat=${pos.lat}&lon=${pos.lon}`);
+      })
+    );
   }
 }
